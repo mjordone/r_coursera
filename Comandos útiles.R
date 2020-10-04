b34438988d5987cf12f307cdc2d0771bb5d7198f
mjordone <martinordonez@gmail.com>
2020-10-03 21:28:14 -0700

    Update

--- Comandos útiles.R
+++ Comandos útiles.R
@@ -58,8 +58,6 @@ is.na()
 is.nan()
 
 # To create data frames
-read.table()
-read.csv()
 data.frame()
 
 x <- data.frame(foo = 1:4, bar = c(T,T,F,F)) # a data frame with two variables: "foo" and "bar".
@@ -77,5 +75,59 @@ x<- list(a=1,b=2,c=3) #a,g,c are the names
 # to name matrices
 m <- matrix(1:4, nrow=2, ncol=2)
 dimnames(m) <- list(c("a","b"), c("c","d")) # las filas son "a" y "b"; las columnas son "c" y "d". dimnames assign a list, where the first element of the list is the vector of the row names and the second element is the vector of the column names
+
+# To read data
+read.table() #e.g., data <- read.table("foo.txt)
+read.csv() # identical to read.table but default separator is a comma
+readLines() # For reading lines of a text file
+source() # For reading in R code files (eg, objects or codes)
+dget() # for reading in R code files
+load() # for reading in saved workspaces
+unserialize() # for reading single R objectis in binary form
+
+# to write data to files
+write.table()
+writeLines()
+dump()
+dput()
+save()
+serialize()
+
+#dput-ting R Objects (basically greating a document that then can be read using dget)
+y <- data.frame(a=1,b="a")
+dput(y)
+Structure(list(a=1,
+               b=Structure(1L, .label="a",
+                           class = "factor")),
+          .Names = c("a","b"), row.names=c(NA,-1L),
+          class = "data.frame")
+dput(y,file="y.R")
+new.y <- dget("y.R")
+
+# Dumping R Objects (difference with dput is that dump can be used on multpl objects. In this example, objects "x" and "y")
+x <- "foo"
+y <- data.frame(a=1, b="a")
+dump(c("x","y"), file="data.R")
+rm(x,y)
+source("data.R")
+
+#file connections (description is the name of the file. open is a code indicating "r" read only, "w" writing, "a" appending)
+str(file)
+function (description = "", open = "", blocking = TRUE,
+         encoding = getOption("encoding"))
+
+con <- file("foo.txt", "r")
+data <- read.csv(con)
+close(con) # all this block is the same as data <-read.csv("foo.txt")
+
+#Connections to read parts of a file (here, first, we are telling R to read the first 10 lines of a gz file. writeLines can be used to write lines back to the text)
+con <- gzfile("words.gz")
+x <- readLines(con,10)
+
+#### same, for url
+con <- url("http://www.jhsph.edu", "r") #"r" is for "reading
+x <- readLines(con)
+head(x)
+
 
 
